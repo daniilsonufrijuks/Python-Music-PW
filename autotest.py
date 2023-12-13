af1d300c521848fd62843db3395fbed48aa80d72
daniilsonufrijuks <do00492@rvt.lv>
2023-12-13 18:44:25 +0200

    final commit

---
+++ autotest.py
@@ -0,0 +1,57 @@
+# . - 0.1 секунды
+# / - 0.5 секунды
+
+# {
+# Z1/5....1...1..5/1/ 5 .... 1 ... 1 .. 5 . 5 / 1 / 5 .... 1 ... 1 .. 5 / 1Z / 5 .... 1 ... 1 .. 5.5/
+# }
+
+from play_sound_by_pygame import play_sound
+from time import sleep
+
+while True:
+    play_sound("126_Fm_TheGridArpBass_02_550.wav")
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.5)
+    play_sound("R8-Snare-3.wav")
+    sleep(0.4)
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.3)
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.2)
+    play_sound("R8-Snare-3.wav")
+    sleep(0.5)
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.5)
+    play_sound("R8-Snare-3.wav")
+    sleep(0.4)
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.3)
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.2)
+    play_sound("R8-Snare-3.wav")
+    sleep(0.1)
+    play_sound("R8-Snare-3.wav")
+    sleep(0.5)
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.5)
+    play_sound("R8-Snare-3.wav")
+    sleep(0.4)
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.3)
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.2)
+    play_sound("R8-Snare-3.wav")
+    sleep(0.5)
+    play_sound("cyborg-03-Plonk.wav")
+    play_sound("126_Fm_TheGridArpBass_02_550.wav")
+    sleep(0.5)
+    play_sound("R8-Snare-3.wav")
+    sleep(0.4)
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.3)
+    play_sound("cyborg-03-Plonk.wav")
+    sleep(0.2)
+    play_sound("R8-Snare-3.wav")
+    sleep(0.1)
+    play_sound("R8-Snare-3.wav")
+    sleep(0.5)
